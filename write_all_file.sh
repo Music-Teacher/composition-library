33308ce5a5c6deaa3aa6581877285861a5896b7a
Olivier Brunel <obrunel@proton.me>
2025-08-30 15:00:29 +0200

    Script to create default txt files next to ALS files

---
+++ write_all_file.sh
@@ -0,0 +1,22 @@
+process () (
+  file_path=$1
+  txt_file="${file_path/".als"/".txt"}"
+  echo $txt_file
+  if [[ -f $txt_file ]]; then
+    echo "File found"
+    cat "$txt_file"
+  else
+    echo "File not found, creating it"
+    echo "$DEFAULT_TEXT" > "$txt_file"
+  fi
+)
+
+export -f process
+
+export DEFAULT_TEXT="""artist:
+name:
+status:
+lyrics:
+extra_info:"""
+
+find -maxdepth 2 -type f -name "*.als" -exec bash -c 'process "{}"' \; 
